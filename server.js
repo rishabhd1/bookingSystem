f8be94d7d65011f2a6b65c1754db3c4f8fa3955d
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-09-10 07:19:37 +0530

    changes required to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.get("/", (req, res) => res.send("API Running"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/booking", require("./routes/api/booking"));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const PORT = process.env.PORT || 5000;
 
