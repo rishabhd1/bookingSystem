adcf12d6dd631f696d31ce6ad9c5b3202bffc597
Rishabh Dubey <rishabhdubey020@gmail.com>
2020-01-02 00:34:34 +0530

    fix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -14,6 +14,7 @@ const User = require("../../models/User");
 router.get("/", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
